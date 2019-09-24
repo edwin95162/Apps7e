b1aa88c08cd8475897f28d45ee7fb7ae6da1b823
edwin <edwingarbol95@hotmail.com>
2019-09-23 21:57:10 -0500

    messenger

--- messenger/manager/models.py
+++ messenger/manager/models.py
@@ -2,3 +2,7 @@ from django.db import models
 
 # Create your models here.
 class main (models.Model):
+    id = models.AutoField(primary_key=True)
+    status = models.BooleanField('Status', default=True)
+    create_date = models.DateField('Creation date', auto_now=False, auto_now_add=True)
+    ###.....
