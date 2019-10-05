98586b52c4b5eea60035173c610cb279831f064d
karen0301 <kbarchik@gmail.com>
2019-10-04 19:59:08 -0500

    error de migrate

--- messenger/manager/admin.py
+++ messenger/manager/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+#importar todos los modelos
+from .models import *
 
-# Register your models here.
+#Registrar modelos
+admin.site.register(category)      #registrar un sitio -->crea administrador de sitio en el servidor
+admin.site.register(author)
+admin.site.register(contact)
+admin.site.register(post)
+admin.site.register(suscriber)
+admin.site.register(web)
+admin.site.register(social)
--- messenger/manager/models.py
+++ messenger/manager/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from ckeditor.fields import RichTextField
+from ckeditor.fields import RichTextField  ##texto enriquecido para HTML
 
 # Create your models here.
 class main (models.Model):
@@ -8,7 +8,7 @@ class main (models.Model):
     create_date = models.DateField('Creation date', auto_now=False, auto_now_add=True)
     modify_date = models.DateField('Modify date', auto_now=False, auto_now_add=True)
     delete_date = models.DateField('Delete date', auto_now=False, auto_now_add=True)
-    ###.....
+    ###..... registros a utlilizar en otra tabla
 
     class Meta: # no crea un modelo.. no se sube con makemigration y no sube a pgadmin.. de uso interno
         abstract = True
@@ -19,7 +19,7 @@ class category(main): #al poner main, esta heredando modelos de la clase main
     class Meta:
         verbose_name = 'Category'
         verbose_name_plural = 'Categories'
-    def __str__(main):
+    def __str__(self):
         return self.name
 
 class author(main):
@@ -27,16 +27,17 @@ class author(main):
     last_name =models.CharField('Last name', max_length=100)
     email = models.EmailField ('E-mail', max_length=150)
     description = models.TextField ('Description')
-    image = models.ImageField ('Author image', null= True, blank=True, upload_to='authors/') #null=true no es necesario poner ese dato en la inscripcion
+    image = models.ImageField ('Author image', null= True, blank=True, upload_to='authors/') #null=true no es necesario poner ese dato en la inscripcion // pillow instalar: imagenes renderizadas
     facebook = models.URLField ('Facebook', null= True,blank=True )
     twitter = models.URLField ('Twitter', null= True,blank=True )
     instagram = models.URLField ('Instagram', null= True,blank=True )
     web = models.URLField ('Web', null= True,blank=True )
+    age = models.CharField('Age', max_length=2)
     class Meta:
         verbose_name = 'Author'
         verbose_name_plural = 'Authors'
     def __str__(self):
-        return '{0},{1}'.format(self.first_name,self.last_name)         #concatenar = interpolar: dos valores se vuelven uno
+        return '{0}{1}{2}'.format(self.first_name," ",self.last_name)         #concatenar = interpolar: dos valores se vuelven uno, se agrega espacio
     
 class post(main):
     title = models.CharField ('Title', max_length=100, unique=True)
@@ -53,3 +54,47 @@ class post(main):
         verbose_name_plural = 'Post'
     def __str__(self):
         return self.title           #pip install django-ckeditor: Utilizado para texto enriquecido
+
+class web(main):
+    about_us = models.TextField ('About us')
+    phone = models.CharField('Phone', max_length=20)
+    email = models.EmailField('E-mail', max_length=150)
+    address = models.CharField('Address', max_length=200)
+    class Meta:
+        verbose_name = 'Web'
+        verbose_name_plural = 'Webs'
+    def __str__ (self):
+        return self.about_us
+    
+class social (main):
+    facebook = models.URLField('Facebook')
+    twitter = models.URLField('Twitter')
+    instagram = models.URLField('Instagram')
+    web = models.URLField('Web')
+    class Meta:
+        verbose_name = 'Social network'
+        verbose_name_plural = 'Social Networks'
+    def __str__ (self):
+        return self.facebook
+
+class contact (main):
+    first_name = models.CharField('First name', max_length=100)
+    last_name = models.CharField('Last name', max_length=100)
+    email = models.EmailField('E-mail', max_length=150)
+    subject = models.CharField('Subject', max_length=100)
+    message = models.TextField('Message')
+    class Meta:
+        verbose_name = 'Contact'
+        verbose_name_plural = 'Contacts'
+    def __str__ (self):
+        return self.subject
+
+class suscriber(main):
+    email = models.EmailField('E-mail', max_length=150)
+    class Meta:
+        verbose_name = 'Suscriber'
+        verbose_name_plural = 'Suscribers'
+    def __str__ (self):
+        return self.email
+
+
